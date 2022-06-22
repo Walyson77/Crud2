7ee62a5edcd0dcc0583cbdac34e68813b6a0d947
email <walyson.castro7@gmail.com>
2022-06-22 20:51:04 -0300

    second commit

---
+++ registra.php
@@ -0,0 +1,19 @@
+<?php
+$nome = !empty($_POST['c1']) ? $_POST['c1'] : '';
+$app = !empty($_POST['c2']) ? $_POST['c2'] : '';
+$apm = !empty($_POST['c3']) ? $_POST['c3'] : '';
+$correo = !empty($_POST['c4']) ? $_POST['c4'] : '';
+
+if($nome&&$app&&$apm&&$correo){
+    include('conexion.php');
+    $consulta=<<<FIN
+    insert into usuarios
+    (nombres,app,apm,correo)
+    values
+    ('$nome','$app','$apm','$correo')
+FIN;
+    if(!mysqli_query($conexion,$consulta)){
+        die('No se pudo agregar el registro');
+    }
+}
+header('Location: lista.php');
